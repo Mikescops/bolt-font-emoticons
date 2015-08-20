e4e254d43298a67f7594f4934f90d74b76500037
Corentin Mors <mikescops@pixelswap.fr>
2015-08-20 14:31:09 +0200

    Fix bug with evil and angel
    Space more space pls !

--- Extension.php
+++ Extension.php
@@ -31,7 +31,7 @@ class Extension extends BaseExtension
     }
 
     function smiley($content) {
-        $smiley_array =  [':)', ':-)', '(-:', '(:', ':smile:', ':(', ':-(', ':sad:',';)', ';-)', ':wink:', ':P', ':-P', ':razz:', '-.-', '-_-', ':sleep:','>:)', '>:-)', ':devil:', ':twisted:',':o', ':-o', ':eek:', '8O', '8o', '8-O', '8-o', ':shock:', ':coffee:', ':D', ':-D', ':grin:','8)', '8-)', 'B)', 'B-)', ':cool:', 'x(', 'x-(', 'X(', 'X-(', ':angry:', 'O:)', '0:)','o:)','O:-)', '0:-)', 'o:-)', ':saint:', ":'(", ":'-(", ':cry:', ':shoot:', '|)', ':squint:', '^^', '^_^', ':lol:', ':/', ':-/', ':beer:'];
+        $smiley_array =  [' :) ', ' :-) ', ' (-: ', ' (: ', ' :smile: ', ' :( ', ' :-( ', ' :sad: ',' ;) ', ' ;-) ', ' :wink: ', ':P', ':-P', ':razz:', '-.-', '-_-', ':sleep:','>:)', '>:-)', ':devil:', ':twisted:',':o', ':-o', ':eek:', '8O', '8o', '8-O', '8-o', ':shock:', ':coffee:', ' :D ', ' :-D ', ':grin:','8)', '8-)', 'B)', 'B-)', ':cool:', 'x(', 'x-(', 'X(', 'X-(', ':angry:', 'O:)', '0:)','o:)','O:-)', '0:-)', 'o:-)', ':saint:', ":'(", ":'-(", ':cry:', ':shoot:', '|)', ':squint:', '^^', '^_^', ':lol:', ':/', ':-/', ':beer:'];
 $smiley_xhtml =  ['happy','happy','happy','happy','happy','unhappy','unhappy','unhappy','wink2','wink2','wink2','tongue','tongue','tongue','sleep','sleep','sleep','devil','devil','devil','devil','surprised','surprised','surprised','surprised','surprised','surprised','surprised','surprised','coffee','grin','grin','grin','sunglasses' ,'sunglasses' ,'sunglasses' ,'sunglasses' ,'sunglasses','angry','angry','angry','angry','angry','saint','saint','saint','saint','saint','saint','saint','cry','cry','cry','shoot','squint','squint','laugh','laugh','laugh','displeased','displeased','beer'];
 
 for ($i = 0; $i<count($smiley_array); $i++)
